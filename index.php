d612771b346e4b544c5a9c3133518e9be219a4a6
Kike <kiketani@kikemonk.com>
2014-12-02 19:43:09 -0600

    Loop de prueba
    Se agrega un loop de prueba para que se vea cómo cargar el contenido de
    los posts con los distintos snippets que se usan en WP.
    
    Los archivos que estaban antes se cambian a loop.php para explicar un
    poco más cómo se usan

--- index.php
+++ index.php
@@ -1,49 +1,62 @@
 <?php get_header(); ?>
 
-	<!-- Loop normal -->
-	<?php if(have_posts()) : ?>
-		<?php while(have_posts()) : the_post(); ?>
-		// this is the inside of the loop
-		<?php endwhile; ?>
-	<?php endif; ?>
+<!-- Ejemplo de un loop normal -->	
+	<?php if (have_posts()) : // Inicio del loop ?>
 	
-	<!-- Loop Multiple -->
-	<?php
-	// The Query
-	$the_query = new WP_Query( $args );
-	// The Loop
-	while ( $the_query->have_posts() ) : $the_query->the_post(); ?>	
-	        
-	        <!--Contenido del post -->  
-	<?php endwhile;
-	// Reset Post Data
-	wp_reset_postdata();
-	?>
-		
-		<!-- Maquetado para artículos -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class($custom-class); ?>>
-		</article>
-		
-		<!--Uso de thumbnails default -->
-		<?php if ( has_post_thumbnail() ) {
-		the_post_thumbnail('large', array('class' => 'img-responsive'));
-		} else { ?>
-		<img src="<?php bloginfo('template_directory'); ?>/images/default-image.jpg" alt="<?php the_title(); ?>" />
-		<?php } ?>
-		
-		<!--Enlaces a post siguiente y anterior -->
-		<?php next_post_link(' %link ') ?>
-		<?php previous_post_link('%link') ?>
-		
-		<!--Navegación -->
-		<?php posts_nav_link(); ?>
-		
+		<?php while (have_posts()) : the_post(); //Loop de TODOS los posts que hay en el sitio ?>
 	
-	
-	
-	<!--Carga de plantilla de comentarios -->
-	<?php comments_template(); ?>
+			<article id="post-<?php the_ID(); ?>" <?php post_class($custom-class); ?>>
+				<header>
+					<h1><?php the_title(); ?></h1>
+					<time datetime="<?php the_time('c'); ?>" pubdate="pubdate"><?php the_time('F jS, Y'); ?></time>
+					<p>by <?php the_author() ?></p>
+				</header>
+				
+				<div class="entry">
+					
+		
+					<!--Uso de thumbnails default -->
+					<?php if ( has_post_thumbnail() ) {
+					the_post_thumbnail('large', array('class' => 'img-responsive'));
+					} else { ?>
+					<img src="<?php bloginfo('template_directory'); ?>/images/default-image.jpg" alt="<?php the_title(); ?>" />
+					<?php } ?>
+					<?php the_content(); ?>
+				</div>
+							
+				<footer id="post-meta-data">
+					<ul class="no-bullet">
+						<li class="add-comment"><?php comments_popup_link('Comentarios', '1 Comentaio', '% Comentarios'); ?></li>
+						<li>Categoría: <?php the_category(', ') ?></li>
+						<li><?php edit_post_link('[Editar]', '<small>', '</small>'); ?></li>
+						<li><?php the_tags('Eriquetas: ', ', ', '<br />'); ?></li>
+					</ul>
+				</footer>
+			
+			</article>
 
-<?php get_sidebar(); ?>
+				
+		<?php endwhile; //Final del loop ?>
+
+		<!--INICIO: navegacióm-->
+		<?php if ( $wp_query->max_num_pages > 1 ) : // si hay más de 1 página se activa la navegación ?>
+			<nav id="page-nav">
+		        <ul class="clearfix">
+			        <li class="next-link"><?php next_posts_link('Página siguiente') ?></li>
+			        <li class="prev-link"><?php previous_posts_link('Página Anterior') ?></li>
+		        </ul>
+	        </nav>
+		<?php endif; ?>
+		<!--FIN: Navegación-->
+		
+<?php else : ?>
+
+	<h2>No se encontraron posts</h2>
+
+<?php endif; //FIN: Loop ?>
+
+<!-- FIN: Ejemplo de loop normal -->
+
+<?php get_sidebar(); //Carga el archivo sidebar.php ?>
 
 <?php get_footer(); ?>

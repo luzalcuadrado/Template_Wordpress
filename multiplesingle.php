ca4c2351c7eb0f0594d8fc222bc1967c57d672b1
Kikemonk <kiketani@kikemonk.com>
2015-11-19 10:17:00 -0600

    Secciones añadidas
    Se añaden secciones que no estaban en la plantilla anterior

--- multiplesingle.php
+++ multiplesingle.php
@@ -1,8 +1,7 @@
+<!-- Si el tema tiene varios singles.php debes renombrar este archivo a single.php -->
 <?php
-    if (in_category(1)) { // Si el post pertenece a la cat 1, redirigir a single-1.php
-    include(TEMPLATEPATH . '/single1.php');
-    } elseif (in_category(2)) { // Si el post pertenece a la cat 2, redirigir a single-2.php
-    include(TEMPLATEPATH . '/single2.php');
-    } else { // Si el post pertenece a la cat 3, redirigir a single-3.php
-    include(TEMPLATEPATH . '/single3.php');
-    } ?>
+	if (in_category($catnumber)) { // Si el post est{a dentro de $catnumber jalará single-$nombrecat.php cómo vista
+		include(TEMPLATEPATH . '/single-$nombrecat.php');
+	} else { 
+	include(TEMPLATEPATH . '/single-default.php'); // Si no está en ninguna categoría en específico este será el default
+} ?>
